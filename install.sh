79ee839e91b27293af36f2d4a035cbe9bc3daf17
Devendra Nishad <108451140+Devendra-kashyap@users.noreply.github.com>
2022-06-30 14:02:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,12 +10,13 @@ pip install instaloader
 rm $PREFIX/bin/termux-tts-speak
 chmod +x termux-tts-speak
 mv -v termux-tts-speak $PREFIX/bin/
-mkdir $PREFIX/.ASSISTENT
-mv -v * .ASSISTENT
+
 mv -v assist $PREFIX/bin
 chmod +x $PREFIX/bin/assist
 clear
 echo "Please subscribe our YT Channel -Dk Tech Point" |lolcat -a
+mkdir $PREFIX/.ASSISTENT
+mv -v * .ASSISTENT
 
 
 
