849a0858b23bc342c57aa0ca02ef0bf8383e3333
Devendra Nishad <ndevendra642@gmail.com>
2024-02-21 23:21:27 +0530

    Version 0.0.2

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 clear
-apt install python python2 git screenfetch ruby termux-api mpv figlet
+apt install python python2 termux-api mpv figlet
 pip install -U deep_translator
-gem install lolcat
-pip install wikipedia
 pip install datetime
 pip install requests
-pip install wikipedia
-pip install instaloader
 rm $PREFIX/bin/termux-tts-speak
 chmod +x termux-tts-speak
 mv -v termux-tts-speak $PREFIX/bin/
@@ -14,7 +10,7 @@ mv -v termux-tts-speak $PREFIX/bin/
 mv -v assist $PREFIX/bin
 chmod +x $PREFIX/bin/assist
 clear
-echo "Please subscribe our YT Channel -Dk Tech Point" |lolcat -a
+echo "Please subscribe our YT Channel -Dk Tech Point"
 mkdir $PREFIX/ASSISTENT
 mv -v * $PREFIX/ASSISTENT
 
